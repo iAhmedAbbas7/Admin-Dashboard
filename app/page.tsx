129f6c94607af19c66ce17937bffcc4ea3b725f1
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-04 15:51:39 +0500

    Add HomePage Component with Basic Structure

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// <== HOMEPAGE COMPONENT ==>
+const HomePage = () => {
+  // RETURNING THE HOMEPAGE CONTENT
+  return (
+    // MAIN CONTAINER
+    <div>HomePage</div>
+  );
+};
+
+// <== EXPORTING THE HOMEPAGE COMPONENT ==>
+export default HomePage;
