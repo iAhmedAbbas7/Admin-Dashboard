d1668c53b9a35d4654263dee8dd0aafb94671b7f
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-04 15:52:18 +0500

    Add ESLint Configuration for TypeScript and Core Web Vitals

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// <== IMPORTS ==>
+import nextTs from "eslint-config-next/typescript";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+// <== ESLINT CONFIGURATION ==>
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // GLOBAL IGNORES
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
+
+// <== EXPORTING ESLINT CONFIGURATION ==>
+export default eslintConfig;
