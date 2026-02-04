52824e90ccce22949cb56a96c476ce615749a76c
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-04 15:53:12 +0500

    Add Initial Next JS Configuration File

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+// <== IMPORTS ==>
+import type { NextConfig } from "next";
+
+// <== NEXT JS CONFIGURATION ==>
+const nextConfig: NextConfig = {};
+
+// <== EXPORTING NEXT JS CONFIGURATION ==>
+export default nextConfig;
