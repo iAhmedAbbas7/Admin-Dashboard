ca9134f61002d7eddf70c34afb02b18a6973171c
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-04 15:52:35 +0500

    Add PostCSS Configuration for Tailwind CSS

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+// <== POSTCSS CONFIGURATION FILE ==>
+const config = {
+  // PLUGINS SECTION
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+// <== EXPORTING POSTCSS CONFIGURATION ==>
+export default config;
