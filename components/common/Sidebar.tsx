0d606d579b3258e4cdbdb1064bc942cd30b34f6b
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-07 14:35:23 +0500

    Add Basic Sidebar Component to the Project

---
+++ components/common/Sidebar.tsx
@@ -0,0 +1,11 @@
+// <== SIDEBAR COMPONENT ==>
+const Sidebar = () => {
+  // RETURNING THE SIDEBAR CONTENT
+  return (
+    // MAIN CONTAINER
+    <div>Sidebar</div>
+  );
+};
+
+// <== EXPORTING THE SIDEBAR COMPONENT ==>
+export default Sidebar;
