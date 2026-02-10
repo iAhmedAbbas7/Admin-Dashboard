8903284a95cf2016f930e705abd79c5243843d30
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-10 16:23:14 +0500

    Introduced the Basic Users Page for the Application

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+// <== IMPORTS ==>
+
+// <== USERS PAGE COMPONENT ==>
+const UsersPage = () => {
+  // RETURNING THE USERS PAGE CONTENT
+  return (
+    // MAIN CONTAINER
+    <div>UsersPage</div>
+  );
+};
+
+// <== EXPORTING THE USERS PAGE COMPONENT ==>
+export default UsersPage;
