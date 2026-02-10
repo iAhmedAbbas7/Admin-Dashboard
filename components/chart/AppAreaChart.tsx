f6ecdcba66905a671d89e13bf38f16dd137b25fe
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-10 16:21:54 +0500

    Introduced the App Area Chart Component for the Application HomePage

---
+++ components/chart/AppAreaChart.tsx
@@ -0,0 +1,111 @@
+// <== CLIENT COMPONENT ==>
+"use client";
+
+// <== IMPORTS ==>
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "@/components/ui/chart";
+import { Users2 } from "lucide-react";
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
+
+// <== CHART DATA ==>
+const CHART_DATA = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+// <== CHART CONFIG ==>
+const CHART_CONFIG = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig;
+
+// <== APP AREA CHART COMPONENT ==>
+const AppAreaChart = () => {
+  // RETURNING THE APP AREA CHART
+  return (
+    // MAIN CONTAINER
+    <div>
+      {/* TITLE */}
+      <h1 className="flex items-center gap-1 text-lg mb-6 font-semibold">
+        <Users2 /> <span>Total Visitors</span>
+      </h1>
+      {/* CHART */}
+      <ChartContainer config={CHART_CONFIG} className="min-h-50 w-full">
+        <AreaChart accessibilityLayer data={CHART_DATA}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis tickLine={false} tickMargin={10} axisLine={false} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <defs>
+            <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--color-desktop)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--color-desktop)"
+                stopOpacity={0.1}
+              />
+            </linearGradient>
+            <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--color-mobile)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--color-mobile)"
+                stopOpacity={0.1}
+              />
+            </linearGradient>
+          </defs>
+          <Area
+            dataKey="mobile"
+            type="natural"
+            fill="url(#fillMobile)"
+            fillOpacity={0.4}
+            stroke="var(--color-mobile)"
+            stackId="a"
+          />
+          <Area
+            dataKey="desktop"
+            type="natural"
+            fill="url(#fillDesktop)"
+            fillOpacity={0.4}
+            stroke="var(--color-desktop)"
+            stackId="a"
+          />
+        </AreaChart>
+      </ChartContainer>
+    </div>
+  );
+};
+
+// <== EXPORTING THE APP AREA CHART COMPONENT ==>
+export default AppAreaChart;
