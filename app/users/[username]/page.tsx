98e35b19bbe2bfce02811fecae2c7294c9e0853c
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-10 16:23:31 +0500

    Introduced the Basic SIngle User Page for the Application

---
+++ app/users/[username]/page.tsx
@@ -0,0 +1,13 @@
+// <== IMPORTS ==>
+
+// <== SINGLE USER PAGE COMPONENT ==>
+const SingleUserPage = () => {
+  // RETURNING THE SINGLE USER PAGE CONTENT
+  return (
+    // MAIN CONTAINER
+    <div>UsersPage</div>
+  );
+};
+
+// <== EXPORTING THE SINGLE USER PAGE COMPONENT ==>
+export default SingleUserPage;
