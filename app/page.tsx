3999a724cb10ad2befdef32bb746824db00192f3
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-10 13:39:37 +0500

    Updated the Application HomePage by Integrating the Testing Layout along with a Bar Chart Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+// <== IMPORTS ==>
+import AppBarChart from "@/components/chart/AppBarChart";
+
 // <== HOMEPAGE COMPONENT ==>
 const HomePage = () => {
   // RETURNING THE HOMEPAGE CONTENT
   return (
     // MAIN CONTAINER
-    <div>HomePage</div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      {/* APP BAR CHART */}
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        Test
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+    </div>
   );
 };
 
