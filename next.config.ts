d9693fcb81875f68453beb3cfbc5bb07932ca12e
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-10 16:16:05 +0500

    Updated the Next Config File to add Image Handling from Pexels Host

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@
 import type { NextConfig } from "next";
 
 // <== NEXT JS CONFIGURATION ==>
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "/photos/**",
+      },
+    ],
+  },
+};
 
 // <== EXPORTING NEXT JS CONFIGURATION ==>
 export default nextConfig;
