deacce47547cb3fa0e91ecadf7c0cae55961ab45
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-02-11 17:14:56 +0500

    Introduced some New Columns for the Table along with Applying Styles to the Table Cells

--- app/payments/columns.tsx
+++ app/payments/columns.tsx
@@ -2,7 +2,18 @@
 "use client";
 
 // <== IMPORTS ==>
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
 // <== PAYMENT TYPE ==>
 export type Payment = {
@@ -21,8 +32,50 @@ export type Payment = {
 // <== COLUMNS ==>
 export const columns: ColumnDef<Payment>[] = [
   {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      // GETTING THE STATUS
+      const status = row.getValue("status");
+      // GETTING THE STATUS CLASS
+      const statusClass =
+        status === "pending"
+          ? "bg-yellow-100 text-yellow-900"
+          : status === "processing"
+            ? "bg-blue-100 text-blue-900"
+            : status === "success"
+              ? "bg-green-100 text-green-900"
+              : "bg-red-100 text-red-900";
+      return (
+        <span
+          className={`uppercase font-semibold w-fit flex items-center justify-center rounded-full px-2 py-1 text-xs ${statusClass}`}
+        >
+          <span>{status as string}</span>
+        </span>
+      );
+    },
   },
   {
     accessorKey: "username",
@@ -30,10 +83,58 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: "email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "amount",
-    header: "Amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      // GETTING THE PAYMENT
+      const payment = row.original;
+      // RETURNING THE DROPDOWN MENU
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
