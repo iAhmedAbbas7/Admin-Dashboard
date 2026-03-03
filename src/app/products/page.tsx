7ac4274084dbde6cdb3c1b8011deb7158d996215
Ahmed Abbas <iahmedabbas7@gmail.com>
2026-03-03 16:01:00 +0500

    Introduced a New Products Page for the Application

---
+++ src/app/products/page.tsx
@@ -0,0 +1,140 @@
+// <== IMPORTS ==>
+import { DataTable } from "./data-table";
+import { Product, columns } from "./columns";
+import { LucideCircleDollarSign } from "lucide-react";
+
+// <== GET DATA FUNCTION ==>
+const getProducts = async (): Promise<Product[]> => {
+  return [
+    {
+      id: 1,
+      name: "Adidas CoreFit T-Shirt",
+      shortDescription:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      description:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      price: 39.9,
+      sizes: ["s", "m", "l", "xl", "xxl"],
+      colors: ["gray", "purple", "green"],
+      images: {
+        gray: "/products/1g.png",
+        purple: "/products/1p.png",
+        green: "/products/1gr.png",
+      },
+    },
+    {
+      id: 2,
+      name: "Puma Ultra Warm Zip",
+      shortDescription:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      description:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      price: 59.9,
+      sizes: ["s", "m", "l", "xl"],
+      colors: ["gray", "green"],
+      images: { gray: "/products/2g.png", green: "/products/2gr.png" },
+    },
+    {
+      id: 3,
+      name: "Nike Air Essentials Pullover",
+      shortDescription:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      description:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      price: 69.9,
+      sizes: ["s", "m", "l"],
+      colors: ["green", "blue", "black"],
+      images: {
+        green: "/products/3gr.png",
+        blue: "/products/3b.png",
+        black: "/products/3bl.png",
+      },
+    },
+    {
+      id: 4,
+      name: "Nike Dri Flex T-Shirt",
+      shortDescription:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      description:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      price: 29.9,
+      sizes: ["s", "m", "l"],
+      colors: ["white", "pink"],
+      images: { white: "/products/4w.png", pink: "/products/4p.png" },
+    },
+    {
+      id: 5,
+      name: "Under Armour StormFleece",
+      shortDescription:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      description:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      price: 49.9,
+      sizes: ["s", "m", "l"],
+      colors: ["red", "orange", "black"],
+      images: {
+        red: "/products/5r.png",
+        orange: "/products/5o.png",
+        black: "/products/5bl.png",
+      },
+    },
+    {
+      id: 6,
+      name: "Nike Air Max 270",
+      shortDescription:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      description:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      price: 59.9,
+      sizes: ["40", "42", "43", "44"],
+      colors: ["gray", "white"],
+      images: { gray: "/products/6g.png", white: "/products/6w.png" },
+    },
+    {
+      id: 7,
+      name: "Nike Ultra Boost Pulse ",
+      shortDescription:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      description:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      price: 69.9,
+      sizes: ["40", "42", "43"],
+      colors: ["gray", "pink"],
+      images: { gray: "/products/7g.png", pink: "/products/7p.png" },
+    },
+    {
+      id: 8,
+      name: "Levi’s Classic Denim",
+      shortDescription:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      description:
+        "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+      price: 59.9,
+      sizes: ["s", "m", "l"],
+      colors: ["blue", "green"],
+      images: { blue: "/products/8b.png", green: "/products/8gr.png" },
+    },
+  ];
+};
+
+// <== PRODUCTS PAGE COMPONENT ==>
+const ProductsPage = async () => {
+  // FETCHING THE PRODUCTS
+  const products = await getProducts();
+  // RETURNING THE PRODUCTS PAGE CONTENT
+  return (
+    // MAIN CONTAINER
+    <div className="mb-8">
+      {/* PAGE HEADER */}
+      <div className="flex items-center gap-2 mb-8 px-4 py-2 bg-secondary rounded-md">
+        <LucideCircleDollarSign />{" "}
+        <h1 className="font-semibold">All Products</h1>
+      </div>
+      {/* DATA TABLE */}
+      <DataTable columns={columns} data={products} />
+    </div>
+  );
+};
+
+// <== EXPORTING THE PRODUCTS PAGE COMPONENT ==>
+export default ProductsPage;
